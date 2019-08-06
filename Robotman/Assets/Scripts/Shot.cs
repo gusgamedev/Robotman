f72ed40da260c18ae42141b69eafbf093d7477eb
Gustavo Larsen <41484330+gusgamedev@users.noreply.github.com>
2019-08-06 16:28:54 -0300

    Add some audios

--- Robotman/Assets/Scripts/Shot.cs
+++ Robotman/Assets/Scripts/Shot.cs
@@ -1,5 +1,26 @@
 using UnityEngine;
 
-public class Shot : MonoBehaviour {
+public class Shot : MonoBehaviour 
+{
+    [SerializeField] private float speed = 20f;
+	[SerializeField] private int damage = 1;
+	[SerializeField] private GameObject _fireParticle;
+	
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        
+		rb2d = GetComponent<Rigidbody2D>();        
+
+        rb2d.velocity = transform.right * speed;
+        Destroy(gameObject, 2f);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) 
+    {
+        //TODO    
+    }
     
+
 }
